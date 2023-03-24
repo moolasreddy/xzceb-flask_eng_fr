ecaac8b35b145128ff3a1e08262b758436dce12c
moolasreddy <54030688+moolasreddy@users.noreply.github.com>
2023-03-24 16:07:45 -0400

    Update translator.py

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -15,33 +15,37 @@ def englishtofrench(englishtext):
      #write the code here
     """This class does from English to French translation"""
 
-    version=''
+    version='2018-05-01'
     authenticator = IAMAuthenticator(apikey)
-    language_translator = LanguageTranslatorV3(version=version,authenticator=authenticator)
+    language_translator = LanguageTranslatorV3(
+                            version=version,
+                            authenticator=authenticator)
     language_translator.set_service_url(url)
-    #lt = language_translator
-    translation = language_translator.translate(text=englishtext, model_id="en-fr").get_result()
+    translation = language_translator.translate(text=englishtext, model_id="en-fr"
+                            ).get_result()
     if englishtext == " ":
         print("Please enter an englishtext to translate.")
     else:
         pass
 
-    return translation['translations'][0]['translation']
+    return translation.get['translations'][0].get['translation']
 
 def frenchtoenglish(frenchtext):
     #write the code here
     """This class does from French to English translation"""
 
-    version=''
+    version='2018-05-01'
     authenticator = IAMAuthenticator(apikey)
-    language_translator = LanguageTranslatorV3(version=version,authenticator=authenticator)
+    language_translator = LanguageTranslatorV3(
+                            version=version,
+                            authenticator=authenticator)
     language_translator.set_service_url(url)
-    #lt = language_translator
-    translation = language_translator.translate(text=frenchtext, model_id="fr-en").get_result()
+
+    translation = language_translator.translate(text=frenchtext, model_id="fr-en"
+                            ).get_result()
     if frenchtext == " ":
         print("Please enter a frenchtext to translate.")
     else:
         pass
 
-    return translation['translations'][0]['translation']
-
+    return translation.get['translations'][0].get['translation']
