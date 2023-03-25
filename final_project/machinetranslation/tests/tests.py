a002df6fc962bf70d0df3f476d4d0c9659a61b40
moolasreddy <54030688+moolasreddy@users.noreply.github.com>
2023-03-25 10:50:07 -0400

    Update tests.py

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -7,9 +7,12 @@ class EnglishToFrenchTest(unittest.TestCase):
     """Class representing a TestCase"""
     def test1(self):
         """    Add the values in ``self``"""
-        self.assertEqual(englishtofrench("Hello"),"Bonjour")
+        self.assertEqual(englishtofrench("Hello"),"Bonjour")   
         self.assertEqual(englishtofrench("Welcome"),"Bienvenue")
         self.assertEqual(englishtofrench("End"),"Fin")
+        self.assertNotEqual(englishtofrench("None"), '')
+        self.assertNotEqual(englishtofrench(0), 0)
+        #self.assertEqual(englishtofrench(""),"erreur")
 
 class FrenchToEnglishTest(unittest.TestCase):
     """Class representing a TestCase"""
@@ -18,5 +21,8 @@ class FrenchToEnglishTest(unittest.TestCase):
         self.assertEqual(frenchtoenglish("Bonjour"),"Hello")
         self.assertEqual(frenchtoenglish("Bienvenue"),"Welcome")
         self.assertEqual(frenchtoenglish("Fin"),"End")
+        self.assertNotEqual(frenchtoenglish("None"), '')
+        self.assertNotEqual(frenchtoenglish(0), 0)
+        #self.assertEqual(frenchtoenglish(""),"erreur")       
 
 unittest.main()
